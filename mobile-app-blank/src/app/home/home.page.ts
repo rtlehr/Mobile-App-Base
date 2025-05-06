d601ab9e418a7ae43886181b56b13015356a2907
Ross Lehr <itsme@rosslehr.com>
2025-05-06 01:24:38 +0000

    add icons

--- mobile-app-blank/src/app/home/home.page.ts
+++ mobile-app-blank/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
+import { IonIcon } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { logoIonic } from 'ionicons/icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ logoIonic });
+  }
 }
