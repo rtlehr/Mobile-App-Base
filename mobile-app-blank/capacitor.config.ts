3dd29fba2e7e495c37e9d79aaf4e85bd4073799b
Ross Lehr <itsme@rosslehr.com>
2025-05-08 23:01:05 +0000

    changed folder name

--- mobile-app-blank/capacitor.config.ts
+++ mobile-app-blank/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rosslehr.mobileappblank',
+  appId: 'com.rosslehr.mobileappbase',
   appName: 'mobile-app-blank',
   webDir: 'www'
 };
--- mobile-app-blank/src/app/app.routes.ts
+++ mobile-app-blank/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./components/home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
@@ -12,18 +12,18 @@ export const routes: Routes = [
   },
   {
     path: 'clock',
-    loadComponent: () => import('./components/clock/clock.page').then( m => m.ClockPage)
+    loadComponent: () => import('./pages/clock/clock.page').then( m => m.ClockPage)
   },
   {
     path: 'files',
-    loadComponent: () => import('./components/files/files.page').then( m => m.FilesPage)
+    loadComponent: () => import('./pages/files/files.page').then( m => m.FilesPage)
   },
   {
     path: 'person',
-    loadComponent: () => import('./components/person/person.page').then( m => m.PersonPage)
+    loadComponent: () => import('./pages/person/person.page').then( m => m.PersonPage)
   },
   {
     path: 'calendar',
-    loadComponent: () => import('./components/calendar/calendar.page').then( m => m.CalendarPage)
+    loadComponent: () => import('./pages/calendar/calendar.page').then( m => m.CalendarPage)
   },
 ];
