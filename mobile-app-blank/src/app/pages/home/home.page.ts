2c4ee33d0d09664a46af222698b572299a0e39d9
Ross Lehr <itsme@rosslehr.com>
2025-05-09 13:23:24 +0000

    added SQLite to development

--- mobile-app-blank/src/app/pages/home/home.page.ts
+++ mobile-app-blank/src/app/pages/home/home.page.ts
@@ -5,11 +5,13 @@ import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { logoIonic } from 'ionicons/icons';
 
+import { SqlTestComponent } from 'src/app/components/sql-test/sql-test.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, SqlTestComponent],
 })
 export class HomePage {
   constructor() {
